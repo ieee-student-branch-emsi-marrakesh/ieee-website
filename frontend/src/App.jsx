cc2a64455cce6274ea68666c3ff36945a9793dfd
Youness SBAI <youness.sbai.work@gmail.com>
2025-10-27 20:19:12 +0100

    Setup tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>IEEE Student Branch EMSI Marrakesh</h1>
+      <h1 className="text-red-500">IEEE Student Branch EMSI Marrakesh</h1>
     </>
   )
 }
